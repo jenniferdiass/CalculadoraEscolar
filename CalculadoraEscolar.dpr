fc9258a0bd547d44ceae8276db1928120247ee4c
jenniferdiass <jennidiias@gmail.com>
2026-05-05 14:56:52 -0300

    Add files via upload

---
+++ CalculadoraEscolar.dpr
@@ -0,0 +1,14 @@
+program CalculadoraEscolar;
+
+uses
+  Vcl.Forms,
+  CalculadoraMediaEscolar in '..\CalculadoraMediaEscolar.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
